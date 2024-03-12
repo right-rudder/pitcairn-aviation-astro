0410fb7732800c204173f49b30b98f9e33c42209
Diego <diegoyon@outlook.com>
2024-03-12 16:20:08 -0600

    add footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Pitcairn Aviation</span>
               <img
-                className="h-10 w-auto"
+                className="h-12 w-auto"
                 src="/src/assets/pitcairn-logo.webp"
                 alt=""
               />
