2fea6d6c421e3687912c14669fa53764a10ca2d7
Diego <diegoyon@outlook.com>
2024-03-21 23:21:30 -0600

    add content and refactor code

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,10 +1,38 @@
+import { BsFuelPumpFill } from "react-icons/bs";
+import { BsFillBoxSeamFill } from "react-icons/bs";
+import { MdEmojiTransportation } from "react-icons/md";
+import { GiSofa } from "react-icons/gi";
+import { ImPower } from "react-icons/im";
+import { MdOutlineSecurity } from "react-icons/md";
+
 const ServiceCard = ({ service }) => {
-  const { title, subtitle } = service;
+  const iconMap = {
+    "Hassle-Free Refueling": BsFuelPumpFill,
+    "Safe Storage Solutions": BsFillBoxSeamFill,
+    "Convenient Transportation": MdEmojiTransportation,
+    "Modern Amenities": GiSofa,
+    "Attentive Ground Support": ImPower,
+    "Peace of Mind": MdOutlineSecurity,
+  };
+
+  const Icon = iconMap[service.title];
+
+  const { title, subtitle, id } = service;
+
   return (
-    <div className="border border-black p-5 bg-white text-center">
-      <h2 className="font-medium text-xl">{title}</h2>
-      <p className="text-gray-500">{subtitle}</p>
-    </div>
+    <a
+      href={`#${id}`}
+      className="relative overflow-hidden cursor-pointer p-5 bg-white border-l-8 border-main-red rounded-xl group"
+    >
+      <div className="bg-main-red absolute h-full w-full top-0 duration-300 right-full group-hover:right-0 z-10"></div>
+      <h2 className="relative font-medium text-2xl z-20 group-hover:text-white duration-100">
+        {title}
+      </h2>
+      <p className="relative text-gray-500 text-lg z-20 group-hover:text-gray-200 duration-100">
+        {subtitle}
+      </p>
+      <Icon className="absolute right-2 text-main-red/20 size-24 -bottom-1 group-hover:text-white/20 z-20 duration-100" />
+    </a>
   );
 };
 
