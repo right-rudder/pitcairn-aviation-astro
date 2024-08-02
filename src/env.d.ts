83859e66ecce5fb334dc1e076082f60f32b78734
Diego <diegoyon@outlook.com>
2024-08-02 16:01:50 -0600

    add contact-us webhook

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly CONTACT_US_FORM_WEBHOOK_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
