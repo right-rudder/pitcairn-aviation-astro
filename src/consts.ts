9bfab0bde0b0eb5b66457451d19d3646411b5752
Diego <diegoyon@outlook.com>
2024-08-04 21:32:37 -0600

    fix consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Pitcairn Aviation';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Pitcairn Aviation";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const PHONE_NUMBER = "800.662.9612";
+export const EMAIL_ADDRESS = "info@pitcairnaviation.com";
